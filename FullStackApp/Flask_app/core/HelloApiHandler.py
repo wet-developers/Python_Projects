c3e662103afd1d0067c15801ef10920925e2b2f5
Austin Pelto <austinpelto16@gmail.com>
2021-07-06 16:25:54 -0400

    flask project

---
+++ FullStackApp/Flask_app/core/HelloApiHandler.py
@@ -0,0 +1,8 @@
+from flask_restful import Api, Resource, reqparse
+
+class HelloApiHandler(Resource):
+    def get(self):
+        return {
+            'resultStatus': 'SUCCESS',
+            'message': "Hello API Handler"
+        }
