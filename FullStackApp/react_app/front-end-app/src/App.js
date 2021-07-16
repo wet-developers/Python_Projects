8fc1db83ca9fb08392dcfd35de25bb11f63fa381
Austin Pelto <austinpelto16@gmail.com>
2021-07-16 14:14:10 -0400

    React Site Updates

--- FullStackApp/react_app/front-end-app/src/App.js
+++ FullStackApp/react_app/front-end-app/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+function showEntry(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {props.email}
+      <ul>
+        <li>{props.fullname}</li>
+        <li>{props.phonenumber}</li>
+      </ul>
     </div>
+  )
+
+}
+function App() {
+  return (
+    <>
+      <showEntry>
+        email = "austinpelto16@gmail.com"
+        fullname = "Austin Pelto"
+        phonenumber = "+7036385794"
+      </showEntry>
+    </>
   );
 }
 
